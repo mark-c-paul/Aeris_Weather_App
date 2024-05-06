c4290af1e167909a5937f478f6bc8beb0e7de9ad
Mark Paul <markcpaul@sympatico.ca>
2024-05-06 13:51:58 -0700

    adding input boxes, a query link and environment variables

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import w_api from "./weatherAPI";
 import Forecast from './Forecast'
 
-
 class WeatherApp extends Component {
   constructor() {
     super();
@@ -27,8 +26,17 @@ class WeatherApp extends Component {
       });
   }
   
+  handleChange = (e) => {
+    this.setState({
+      code: e.target.code,
+      format: e.target.format,
+      filter: e.target.filter,
+      limit: e.target.limit
+    });
+  };
+  
   render() {
-    const { data, weekly_forecast, clicked } = this.state;
+    const { data, weekly_forecast, clicked} = this.state;
     console.log("Data Collected", weekly_forecast, clicked);
     return (
     <div className="wrapper">
@@ -36,14 +44,48 @@ class WeatherApp extends Component {
       <form>
       <fieldset>
          <div class="form-group">
-           <label for="code">Postal/Zip Code: </label>
-           <input name="location" 
+           <label for="Code">Postal/Zip Code: </label>
+           <input id="inputCode"
+                  type="text"           
                   size="6" 
                   placeholder="V1B3K4"
                   minlength="5"
-                  maxlength="6"/>
+                  maxlength="6"
+                  onChange={this.handleChange}
+                  value={this.state.code}
+                  />
+           <label for="Format">  Format: </label>
+           <input id="inputFormat"
+                  type="text"
+                  size="6" 
+                  placeholder="json"
+                  onChange={this.handleChange}
+                  value={this.state.format}
+                  />
+           <label for="Filter">  Filter: </label>
+           <input id="inputFilter"
+                  type="text"
+                  size="6" 
+                  placeholder="days"
+                  onChange={this.handleChange}
+                  value={this.state.filter}
+                  />
+           <label for="Limit">  Limit: </label>
+           <input id="inputLimit"
+                  type="text"
+                  size="6" 
+                  placeholder="7"
+                  onChange={this.handleChange}
+                  value={this.state.limit}
+                  />
          </div>
        </fieldset>
+       <h3 for="Query"> 
+       Query: <a href = {'https://api.aerisapi.com/forecasts/'
+       + this.inputCode + '?&format=' + this.inputFormat + '&filter=' + this.inputFilter + '&limit=' + this.inputLimit +
+       '&client_id=Cr0YRR2ZeUaVOkc1dzisr&client_secret=5KmCGBqh2wOaaDXRbUE2xa1SyqniaC6fCqUk1R1j'
+       }>Link</a>
+       </h3>
        <p>
        <button type="submit">Submit</button>
        </p>
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,8 +1,16 @@
-import React, {Component} from 'react'
 import axios from "axios"
 
+var zip = 'V1B3K4';
+var path = 'https://api.aerisapi.com/forecasts/';
+var clientID = '&client_id=Cr0YRR2ZeUaVOkc1dzisr';
+var clientSecret = '&client_secret=5KmCGBqh2wOaaDXRbUE2xa1SyqniaC6fCqUk1R1j';
+var rest = '?&format=json&filter=day&limit=7';
+var format = '?&format=json';
+var filter = '&filter=day';
+var limit = '&limit=7';
+var query = path + zip + format + filter + limit + clientID + clientSecret;
 const getWeatherData = () => axios
-    .get(`https://api.aerisapi.com/forecasts/V1B3K4?&format=json&filter=day&limit=7&client_id=Cr0YRR2ZeUaVOkc1dzisr&client_secret=5KmCGBqh2wOaaDXRbUE2xa1SyqniaC6fCqUk1R1j`)
+    .get(query)
 
 
 
